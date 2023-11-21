625ecb6c20b9df6ee44d7641b7e71f30eeb33dbd
Samuel Siesjo <samsie0717@skola.goteborg.se>
2023-11-21 13:53:20 +0100

    inl4 aop timing advice

---
+++ src/main/java/com/yrgo/advice/PerformanceTimingAdvice.java
@@ -0,0 +1,25 @@
+package com.yrgo.advice;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PerformanceTimingAdvice {
+    public Object performanceTimingMeasurement (ProceedingJoinPoint method) throws Throwable {
+        long startTime = System.nanoTime();
+
+        try{
+            Object value = method.proceed();
+            return value;
+        } finally {
+            long endTime = System.nanoTime();
+            long timeTaken = endTime - startTime;
+            System.out.println("Method: " + method.getSignature().getName() + " took " + timeTaken/1000000 + "ms");
+        }
+    }
+
+    public void beforeAdviceTesting(JoinPoint jp) {
+        System.out.println("Now enterinng method: " + jp.getSignature().getName());
+    }
+}
